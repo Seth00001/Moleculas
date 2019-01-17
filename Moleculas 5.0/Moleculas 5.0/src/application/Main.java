2a2ad2421033944ed1c015cf88ddbe4cd4c601a5
g.stadnichenko <g.stadnichenko@temabit.com>
2019-01-17 18:08:07 +0200

    experiment preset

--- Moleculas 5.0/Moleculas 5.0/src/application/Main.java
+++ Moleculas 5.0/Moleculas 5.0/src/application/Main.java
@@ -8,12 +8,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Grid grid = new Grid(100, 100, 100);
+		Grid grid = new Grid(200, 200, 200);
 		GridHelper helper = new GridHelper();
 		helper.grid = grid;
 		
 		
-		InitialConditionProvider conditionProvider = new InitialConditionProvider(20, 20);
+		InitialConditionProvider conditionProvider = new InitialConditionProvider(15, 17);
 		conditionProvider.setupEnvironment(grid);
 		
 //		for(int i = 0; i < grid.dimX ; i++) {
